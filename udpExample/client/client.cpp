983f93f6dc4417837d5d649bc61249667a95429d
Alex Maksimov <almaximort@yandex.ru>
2022-04-23 10:21:55 +0300

    Add udp client and server example for linux

---
+++ udpExample/client/client.cpp
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+  char msg1[] = "MESSAGE1";
+  char msg2[] = "MESSAGE2";
+  int sock;
+  struct sockaddr_in addr;
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(3425);
+  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  sendto(sock, msg1, sizeof(msg1), 0, (struct sockaddr *)&addr, sizeof(addr));
+  close(sock);
+  return 0;
+}
---
+++ udpExample/server/server.cpp
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+int main() {
+  int sock;
+  struct sockaddr_in addr;
+  char buf[1024];
+  int bytes_read;
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+  
+  ddr.sin_family = AF_INET;
+  addr.sin_port = htons(3425);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("bind");
+    return 1;
+  }
+  while(1) {
+    bytes_read = recvfrom(sock, buf, 1024, 0, NULL, NULL);
+    buf[bytes_read] = '\0';
+    printf("%s\n", buf);
+  }
+}
